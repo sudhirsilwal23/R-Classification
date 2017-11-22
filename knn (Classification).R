9cb9a7a3723d7f0307c473776b09df1b6bea8c46
SUDHIR SILWAL <sudhirsilwal23@gmail.com>
2017-11-22 21:19:03 +0530

    Add files via upload

---
+++ knn (Classification).R
@@ -0,0 +1,75 @@
+#################################################################################
+#####################  K-Nearest Neighbours (K-NN) - R  #########################
+#################################################################################
+
+---------------------------------------------------------------------------------
+# Step : 1 Installing the Packages
+---------------------------------------------------------------------------------
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+# install.packages('ElemStatLearn')
+library(ElemStatLearn)
+
+---------------------------------------------------------------------------------
+# Step : 2 Data Preprocessing
+#--------------------------------------------------------------------------------
+         #2(a) Importing the dataset
+dataset = read.csv('Social_Media_Advertisement.csv')
+dataset = dataset[3:5]
+
+         #2(b) Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+         #2(c) Splitting the dataset into the Training set and Test set
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+         #2(d) Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+--------------------------------------------------------------------------------
+# Step : 3 Data modelling
+--------------------------------------------------------------------------------
+       #3(a) Fitting K-NN to the Training set and Predicting the Test set results
+          library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
+
+        #3(b) Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+        #3(c) Visualising the Training set results
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+plot(set[, -3],
+     main = 'K-NN (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+         #3(d) Visualising the Test set results
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+plot(set[, -3],
+     main = 'K-NN (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
